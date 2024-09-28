5ff8826d73f7012f24dbadb281f50d276a526e1c
Ryan Morgan <RyanOnGithub@gmail.com>
2024-09-28 16:22:23 -0700

    Remove nextjs boilerplate

---
+++ app/layout.tsx
@@ -0,0 +1,21 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Quote Machine",
+  description: "A random quote generator",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
