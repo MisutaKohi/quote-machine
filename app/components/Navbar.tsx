74d2f88c0204a94b38ade5ba9bd327105b40f492
Ryan Morgan <RyanOnGithub@gmail.com>
2024-10-02 20:59:43 -0700

    Affix Navbar to top of page and hide scroll overflow

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 const Navbar = () => {
   return (
-    <nav className="bg-gray-800 p-4">
+    <nav className="bg-gray-800 p-4 fixed top-0 left-0 w-full z-50">
       <div className="container mx-auto flex justify-between items-center">
         <div className="text-white text-lg font-bold">
           <Link href="/">Quote Machine</Link>
